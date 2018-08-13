2b5334816e5f6519a712dd45b1bc78826b6c4f49
Andrey Gushchin <andreygushchin@Andreys-MacBook-Pro-2.local>
2018-08-12 17:25:53 -0700

    DB setup and first migration:

--- intproject/core/models.py
+++ intproject/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    id = models.CharField(primary_key=True, max_length=30)
+    question_text = models.CharField(max_length=1000)
+    complexity = models.IntegerField()
